aa4c121ffe63f5289c9d027469779c4de720d9fe
Klyucherov Evgeniy <Klyucherov.e@yandex.ru>
2022-08-29 09:05:23 +0300

    V1

--- README.md
+++ README.md
@@ -1,2 +1,123 @@
-# api_final
-api final
+# api_final_yatube
+
+### Описание
+
+Проект добавляет возможность социальной сети Yatube  
+быть интегрированной в сторонние приложения благодаря сервису REST API.
+
+### Установка
+
+Скопируйте проект на свой компьютер:
+
+```
+git clone https://github.com/Klyucherov/api_final_yatube.git
+```
+
+Cоздайте и активируйте виртуальное окружение для этого проекта:
+
+```
+python -m venv venv
+```
+
+```
+source env/bin/activate
+```
+
+Установите зависимости из файла requirements.txt:
+
+```
+python -m pip install --upgrade pip
+```
+
+```
+pip install -r requirements.txt
+```
+
+Перейдите в директорию проекта:
+
+```
+cd yatube_api
+```
+
+Выполните миграции:
+
+```
+python manage.py migrate
+```
+
+Запустите проект:
+
+```
+python manage.py runserver
+```
+
+### Примеры
+
+Пользователь аутентифицируется посредвстом JWTAuthentication.  
+Получите токен.  
+Отправьте POST запрос на URL:
+
+```
+http://127.0.0.1:8000/api/v1/auth/jwt/create/
+```
+
+Получение публикаций GET запрос:
+
+```
+http://127.0.0.1:8000/api/v1/posts/
+```
+
+Создание публикации POST запрос:
+
+```
+http://127.0.0.1:8000/api/v1/posts/
+```
+
+```
+{
+"text": "string"
+}
+```
+
+Получение одной публикации GET запрос:
+
+```
+http://127.0.0.1:8000/api/v1/posts/{id}/
+```
+
+Обновление публикации PUT запрос:
+
+```
+http://127.0.0.1:8000/api/v1/posts/{id}/
+```
+
+```
+{
+"text": "string"
+}
+```
+
+Частичное обновление публикации PATHС запрос:
+
+```
+http://127.0.0.1:8000/api/v1/posts/{id}/
+```
+
+```
+{
+"group": 0
+}
+```
+
+Удаление публикации DELETE запрос:
+
+```
+http://127.0.0.1:8000/api/v1/posts/{id}/
+```
+
+### В проекте использованы технологии:
+
+- Python
+- Django
+- Django REST Framework
+- JWT + Djoser
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,55 @@
-# TODO:  Напишите свой вариант
+from api.mixins import ListCreateViewSet
+from api.permissions import IsAuthorOrReadOnly
+from api.serializers import (
+    PostSerializer, CommentSerializer, GroupSerializer, FollowSerializer
+)
+from posts.models import Post, Group
+from rest_framework.filters import SearchFilter
+from rest_framework.generics import get_object_or_404
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.permissions import AllowAny
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
+
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    pagination_class = LimitOffsetPagination
+    permission_classes = (IsAuthorOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (IsAuthorOrReadOnly,)
+
+    def get_queryset(self):
+        post_id = self.kwargs.get('post_id')
+        post_obj = get_object_or_404(Post, id=post_id)
+        post_comments = post_obj.comments.all()
+        return post_comments
+
+    def perform_create(self, serializer):
+        post_id = self.kwargs.get('post_id')
+        post_obj = get_object_or_404(Post, id=post_id)
+        serializer.save(author=self.request.user, post=post_obj)
+
+
+class GroupViewSet(ReadOnlyModelViewSet):
+    serializer_class = GroupSerializer
+    queryset = Group.objects.all()
+    permission_classes = (AllowAny,)
+
+
+class FollowViewSet(ListCreateViewSet):
+    serializer_class = FollowSerializer
+    filter_backends = (SearchFilter,)
+    search_fields = ('following__username',)
+
+    def get_queryset(self):
+        return self.request.user.follower.all()
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
